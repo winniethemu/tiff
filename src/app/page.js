3f1fcc9cc0e4a9c42723f65b1df2820ce36a000a
Mu Ye <emailyemu@gmail.com>
2023-05-30 09:46:36 +0800

    Comment out unused import

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';

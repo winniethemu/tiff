fefb775e6a935ee22911823867f14e68fd92c696
Mu Ye <emailyemu@gmail.com>
2023-05-30 10:16:41 +0800

    Next config generates repo specific paths

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+let assetPrefix = '';
+let basePath = '';
+
+if (process.env.GITHUB_ACTIONS) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
-  output: 'export',
+  assetPrefix,
+  basePath,
   images: { unoptimized: true },
+  output: 'export',
 };
 
 module.exports = nextConfig;
